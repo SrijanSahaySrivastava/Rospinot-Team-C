d932df07950adeff31c1526d1e9453751b642e5f
Srijan Sahay Srivastava <70461251+SrijanSahaySrivastava@users.noreply.github.com>
2023-01-08 21:47:01 +0530

    Update ~ Srijan
    added documented lines of code as suggested and uploaded final code
    
    • needs optimisations
    • works with mp3 files
    • libraries used :-
             ⌊ PyGame
             ⌊ SenseHat
             ⌊ Time

--- audio.py
+++ audio.py
@@ -6,17 +6,28 @@ sense = SenseHat()
 
 #pulling audio file [works sometimes :( ]
 mixer.init()
-mixer.music.load('filename.mp3')
-mixer.music.play(-1)
+mixer.music.load('filename.mp3')  #doc > replace filename.mp3 with path of og file
+mixer.music.play(-1)  #doc > infinte loop
 
 sense.clear(0, 0, 0)
 
 while True:
-  spectrum = mixer.music.get_volume() #multiply by ~7 to zoom in or zoom out audio spectrum
+  spectrum = mixer.music.get_volume() * 255 #Bahut error deta hai, kripya change mat karna
   
   sense.clear(0, 0, 0)
 
   for i in range(8):
+    #doc > show pixels with audio spectrum
     sense.set_pixel(i, 7, (spectrum, 0, 0))
+    sense.set_pixel(i, 6, (spectrum, 0, 0))
+    sense.set_pixel(i, 5, (spectrum, 0, 0))
+    sense.set_pixel(i, 4, (spectrum, 0, 0))
+    sense.set_pixel(i, 3, (spectrum, 0, 0))
+    sense.set_pixel(i, 2, (spectrum, 0, 0))
+    sense.set_pixel(i, 1, (spectrum, 0, 0))
+    sense.set_pixel(i, 0, (spectrum, 0, 0))
+
+  #time.sleep(0.1)
     
+
 #bhagwan ke naame pe kaam karna, nariyal chadhaunga
